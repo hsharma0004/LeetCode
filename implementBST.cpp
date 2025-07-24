a096dd07b0c7392f114e3a8ecf020c4a18ea05a7
Harsh Sharma <123738953+hsharma0004@users.noreply.github.com>
2025-07-24 22:25:29 +0530

    Update implementBST.cpp
    Added Delete functionality

--- implementBST.cpp
+++ implementBST.cpp
@@ -52,6 +52,78 @@ void levelwiseTraversal(Node *root)
   }
 }
 
+Node *minval(Node *root)
+{
+  Node *temp = root;
+
+  while (temp->left != NULL)
+  {
+    temp = temp->left;
+  }
+  return temp;
+}
+
+Node *deleteFromBST(Node *root, int val)
+{
+  // base case
+  if (root == NULL)
+    return root;
+
+  if (root->data == val)
+  {
+    // 0 child
+    if (root->left == NULL && root->right == NULL)
+    {
+      delete root;
+      return NULL;
+    }
+    // 1 child
+    // left child exists
+    if (root->left != NULL && root->right == NULL)
+    {
+      Node *temp = root->left;
+      delete root;
+      return temp;
+    }
+    // right child exists
+    if (root->left == NULL && root->right != NULL)
+    {
+      Node *temp = root->right;
+      delete root;
+      return temp;
+    }
+    // 2 child
+    if (root->left != NULL && root->right != NULL)
+    {
+      int mini = minval(root->right)->data;
+      root->data = mini;
+      root->right = deleteFromBST(root->right, mini);
+    }
+  }
+  else if (root->data > val)
+  {
+    root->left = deleteFromBST(root->left, val);
+    return root;
+  }
+  else
+  {
+    root->right = deleteFromBST(root->right, val);
+    return root;
+  }
+}
+
+void inorderTraversal(Node *root)
+{
+  if (root == NULL)
+    return;
+  // LNR
+  // Recursion
+  // Inorder of BST is sorted
+  inorderTraversal(root->left);
+  cout << root->data << " ";
+  inorderTraversal(root->right);
+}
+
 Node *insertIntoBST(Node *root, int d)
 {
   // base case
@@ -89,6 +161,18 @@ int main()
   Node *root = NULL;
   cout << "Enter the Data: " << endl;
   TakeInput(root);
-  cout<<"Printing BST : "<<endl;
+  cout << "Printing BST : " << endl;
+  levelwiseTraversal(root);
+
+  cout << "Inorder BST : " << endl;
+  inorderTraversal(root);
+  cout<<"\n";
+
+  cout<<"Min Value is: "<<minval(root)->data<<endl;
+
+  // Deletion
+  root = deleteFromBST(root , 30);
+
+  cout << "Printing BST : " << endl;
   levelwiseTraversal(root);
 }
